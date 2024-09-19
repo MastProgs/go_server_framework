841117643f24471f9ab0941b8b4dfa5ebeca7bf4
김형준 <khjkhj2804@naver.com>
2024-09-19 13:12:39 +0900

    예제 리팩토링

--- handlers/test_handlers.go
+++ handlers/test_handlers.go
@@ -4,27 +4,15 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"time"
 )
 
-func SlowHandler(r *http.Request) (interface{}, error) {
-	time.Sleep(2 * time.Second)
-	data := map[string]string{"msg": "느린 작업 완료!"}
+func PingHandler(r *http.Request) (interface{}, error) {
+	data := map[string]string{"msg": "pong"}
 	return data, nil
 }
 
-func FastHandler(r *http.Request) (interface{}, error) {
-	data := map[string]string{"msg": "빠른 응답!"}
-	return data, nil
-}
+func PostPingHandler(r *http.Request) (interface{}, error) {
 
-func GetProfileHandler(r *http.Request) (interface{}, error) {
-	// GET 요청 처리 로직
-	return map[string]string{"name": "John Doe", "email": "john@example.com"}, nil
-}
-
-func UpdateProfileHandler(r *http.Request) (interface{}, error) {
-	// POST 요청 처리 로직
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read request body: %v", err)
@@ -34,7 +22,17 @@ func UpdateProfileHandler(r *http.Request) (interface{}, error) {
 	// body 출력
 	fmt.Printf("Received body: %s\n", string(body))
 
-	// 여기에 body 처리 로직 추가
+	data := map[string]string{"msg": "pong"}
+	return data, nil
+}
 
-	return map[string]string{"message": "Profile updated successfully"}, nil
+func GetProfileHandler(r *http.Request) (interface{}, error) {
+	// GET 요청 처리 로직
+	return map[string]string{"name": "John Doe", "email": "john@example.com"}, nil
+}
+
+func UpdateProfileHandler(r *http.Request) (interface{}, error) {
+
+	// 여기에 body 처리 로직 추가
+	return map[string]string{"msg": "Profile updated successfully"}, nil
 }
--- router/router.go
+++ router/router.go
@@ -21,25 +21,21 @@ func SetupRouter() *http.ServeMux {
 	apiHandler := http.NewServeMux()
 	mux.Handle("/api/", http.StripPrefix("/api", apiHandler))
 
+	// /api/ping
+	apiHandler.Handle("/ping", middleware.WorkerPoolMiddleware(Pool, middleware.MethodHandler{
+		Get:  handlers.PingHandler,
+		Post: handlers.PostPingHandler,
+	}))
+
 	// 인증이 필요한 API 그룹
 	authApiHandler := http.NewServeMux()
 	apiHandler.Handle("/auth/", middleware.JWTAuthMiddleware(http.StripPrefix("/auth", authApiHandler)))
 
 	// 인증이 필요한 API 엔드포인트
-	// /auth/profile
+	// /api/auth/profile
 	authApiHandler.Handle("/profile", middleware.WorkerPoolMiddleware(Pool, middleware.MethodHandler{
 		Get:  handlers.GetProfileHandler,
 		Post: handlers.UpdateProfileHandler,
-		// 필요한 다른 메서드들...
-	}))
-
-	// 공개 API 엔드포인트
-	// /api/slow
-	apiHandler.Handle("/slow", middleware.WorkerPoolMiddleware(Pool, middleware.MethodHandler{
-		Get: handlers.SlowHandler,
-	}))
-	apiHandler.Handle("/fast", middleware.WorkerPoolMiddleware(Pool, middleware.MethodHandler{
-		Get: handlers.FastHandler,
 	}))
 
 	// 인증 관련 엔드포인트
